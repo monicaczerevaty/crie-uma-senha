c9ab2e03898290e6298e0e9b13164de3b5d531c0
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-05-21 11:40:36 -0300

    2105

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ let tamanhoSenha = 12;
 senha.textContent = tamanhoSenha;
 const botoes = document.querySelector('.parametro-senha-botao');
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
-
+    if(tamanhoSenha > 1){
+tamanhoSenha = tamanhoSenha - 1;
+    }
+numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+    tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
