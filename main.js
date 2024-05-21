3fb099ae6fb44d4a34033cc91b92f231c9fa6946
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-05-21 11:31:56 -0300

    2105

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.parametro-senha-botao');
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 12;
+senha.textContent = tamanhoSenha;
+const botoes = document.querySelector('.parametro-senha-botao');
+botoes[0].onclick = diminuiTamanho;
+function diminuiTamanho(){
+
+}
